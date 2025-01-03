a934cecc27535a619abce6ef75b74fe013890eb1
deanstein <jdeangoldstein@gmail.com>
2025-01-03 15:05:51 -0700

    Moving some copy from old contact page to new contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -7,18 +7,22 @@
 		JDGIFrame
 	} from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../image-attributes-collection';
-	import { jdgSharedUrls } from 'jdg-ui-svelte/jdg-shared-strings.js';
 	import UpNext from '../../components/UpNext.svelte';
 </script>
 
 <JDGContentContainer>
-	<JDGContentBoxFloating title={'GET IN TOUCH'} subtitle={'Questions? Comments?'}>
+	<JDGContentBoxFloating title={'GET IN TOUCH'} subtitle={"We'd love to hear from you"}>
+		<JDGBodyCopy paddingTop="0" paddingBottom="0" textAlign="center">
+			Feedback on our products? Custom design request?
+			<br />
+			Fill out the form below!
+		</JDGBodyCopy>
 		<JDGBodyCopy paddingTop="0" paddingBottom="0" textAlign="center">
 			<h3 style="margin-bottom: 10px;">CONTACT FORM</h3>
 		</JDGBodyCopy>
 		<div class="iframe-wrapper">
 			<JDGIFrame
-				title={'Contact Joshua Dean Goldstein'}
+				title={'Contact Parametrix3D'}
 				src={'https://docs.google.com/forms/d/e/1FAIpQLSeQHueNHg-f5Ad6jN7jdW45jQXgQMxYv-mjZy11uLyJgak26Q/viewform?embedded=true'}
 				maxHeightPx="900"
 			/>
