890eaedd7c10636928b9c7a6001f7a9f3772b331
deanstein <jdeangoldstein@gmail.com>
2025-01-13 21:43:46 -0700

    Adding ProductCodeLabel on all ImageTiles

---
+++ src/components/ProductCodeLabel.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let productCode = '000';
+</script>
+
+<div class="pmx-product-code-label-container">
+	<div class="product-code-label">
+		#{productCode}
+	</div>
+</div>
+
+<style>
+	.pmx-product-code-label-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 0 0 0 12px;
+	}
+
+	.product-code-label {
+		font-family: 'REM';
+		font-style: italic;
+		font-weight: 100;
+		color: #565656ff;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,7 @@
 		JDGImageTile
 	} from 'jdg-ui-svelte';
 	import UpNext from '../components/UpNext.svelte';
+	import ProductCodeLabel from '../components/ProductCodeLabel.svelte';
 
 	const imageHeightLg = 350;
 	const imageHeightSm = 250;
@@ -58,36 +59,43 @@
 				label={pageMeta.products.CITYSCAPE_021.title.toUpperCase()}
 				imageAttributes={imageAttributesCollection.products.CITYSCAPE_021A_0}
 				href={pageMeta.products.CITYSCAPE_021.href}
-			/>
+			>
+				<ProductCodeLabel productCode={productMeta.CITYSCAPE_021.displayCode} />
+			</JDGImageTile>
 			<JDGImageTile
 				label={pageMeta.products.DECOR_030.title.toUpperCase()}
 				imageAttributes={imageAttributesCollection.products.DECOR_030A_0}
 				href={pageMeta.products.DECOR_030.href}
 				objectPosition="left"
-			/>
+				><ProductCodeLabel productCode={productMeta.DECOR_030.displayCode} /></JDGImageTile
+			>
 			<JDGImageTile
 				label={pageMeta.products.PLANTER_003.title.toUpperCase()}
 				imageAttributes={imageAttributesCollection.products.PLANTER_003F_0}
 				href={pageMeta.products.PLANTER_003.href}
 				objectPosition="left top"
-			/>
+				><ProductCodeLabel productCode={productMeta.PLANTER_003.displayCode} /></JDGImageTile
+			>
 			<JDGImageTile
 				label={pageMeta.products.PEN_HOLDER_008.title.toUpperCase()}
 				imageAttributes={imageAttributesCollection.products.PEN_HOLDER_008I_0}
 				href={pageMeta.products.PEN_HOLDER_008.href}
-			/>
+				><ProductCodeLabel productCode={productMeta.PEN_HOLDER_008.displayCode} /></JDGImageTile
+			>
 			<JDGImageTile
 				label={pageMeta.products.CARD_HOLDER_015.title.toUpperCase()}
 				imageAttributes={imageAttributesCollection.products.CARD_HOLDER_015C_0}
 				href={pageMeta.products.CARD_HOLDER_015.href}
 				objectPosition="left top"
-			/>
+				><ProductCodeLabel productCode={productMeta.CARD_HOLDER_015.displayCode} /></JDGImageTile
+			>
 			<JDGImageTile
 				label={productMeta.MAGNET_047.variants.B.name.toUpperCase()}
 				imageAttributes={imageAttributesCollection.products.MAGNET_DEN_CO_047BC_0}
 				href={pageMeta.products.MAGNET_047.href}
 				objectPosition="top"
-			/>
+				><ProductCodeLabel productCode={productMeta.MAGNET_047.displayCode} /></JDGImageTile
+			>
 			<JDGImageTile
 				label={productMeta.MAGNET_047.variants.D.name.toUpperCase()}
 				imageAttributes={imageAttributesCollection.products.MAGNET_CO_TOPO_047D_0}
@@ -95,23 +103,27 @@
 					'#' +
 					productMeta.MAGNET_047.variants.D.displayCode}
 				objectPosition="left bottom"
-			/>
+				><ProductCodeLabel productCode={productMeta.MAGNET_047.displayCode} /></JDGImageTile
+			>
 			<JDGImageTile
 				label={productMeta.GRINDER_039.name.toUpperCase()}
 				imageAttributes={imageAttributesCollection.products.GRINDER_039A_0}
 				href={pageMeta.products.GRINDER_039.href}
-			/>
+				><ProductCodeLabel productCode={productMeta.GRINDER_039.displayCode} /></JDGImageTile
+			>
 			<JDGImageTile
 				label={productMeta.COASTER_028.name.toUpperCase()}
 				imageAttributes={imageAttributesCollection.products.COASTER_028A_0}
 				href={pageMeta.products.COASTER_028.href}
-			/>
+				><ProductCodeLabel productCode={productMeta.COASTER_028.displayCode} /></JDGImageTile
+			>
 			<JDGImageTile
 				label={productMeta.KITCHEN_CADDY_007.name.toUpperCase()}
 				imageAttributes={imageAttributesCollection.products.KITCHEN_CADDY_007F_0}
 				href={pageMeta.products.KITCHEN_CADDY_007.href}
 				objectPosition="left bottom"
-			/>
+				><ProductCodeLabel productCode={productMeta.KITCHEN_CADDY_007.displayCode} /></JDGImageTile
+			>
 		</JDGGridLayout>
 	</JDGContentBoxFloating>
 	<UpNext />
